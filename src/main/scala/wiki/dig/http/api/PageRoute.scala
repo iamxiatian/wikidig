4eb0142268b0d1fd7253e0206b8cee517e43e1d6
xiatian <xiatian1119@gmail.com>
2019-12-20 12:47:21 +0800

    update wiki info

--- src/main/scala/wiki/dig/http/api/PageRoute.scala
+++ src/main/scala/wiki/dig/http/api/PageRoute.scala
@@ -1,19 +1,62 @@
 package wiki.dig.http.api
 
-import spark.{Request, Response, Route}
-import wiki.dig.util.Logging
-import spark.Spark._
-import wiki.dig.store.db.{PageContentDb, PageDb}
 import io.circe.syntax._
+import spark.Spark._
+import spark.{Request, Response, Route}
+import wiki.dig.store.db.{PageContentDb, PageDb}
+import wiki.dig.util.Logging
 
 object PageRoute extends JsonSupport with Logging {
   def register(): Unit = {
     //获取账号根据邮箱
-    get("/api/wiki/page/:name", "application/json", showPage)
+    get("/wiki/page", "text/html", showPageHtml)
+    get("/wiki/page_content", "text/html", showPageContent)
   }
 
+  private def showPageContent: Route = (request: Request, _: Response) => {
+    Option(request.queryMap("id").value()).flatMap(_.toIntOption) match {
+      case Some(id) =>
+        PageContentDb.getContent(id).getOrElse("")
+      case None =>
+        ""
+    }
+  }
 
-  private def showPage: Route = (request: Request, _: Response) => {
+  def showPageHtml: Route = (request: Request, _: Response) => {
+    //val name = Option(request.params(":name")).getOrElse("").trim
+    val name = Option(request.queryMap("name").value()).getOrElse("").trim
+    PageDb.getIdByName(name) match {
+      case Some(pageId) =>
+        val inlinks = PageDb.getInlinks(pageId).flatMap {
+          id =>
+            PageDb.getNameById(id).map {
+              linkName =>
+                s"""<li><a href="?name=${linkName}">${linkName}</a></li>"""
+            }
+        }.mkString("<ul>", "\n", "</ul>")
+
+        val outlinks = PageDb.getInlinks(pageId).flatMap {
+          id =>
+            PageDb.getNameById(id).map {
+              linkName =>
+                s"""<li><a href="?name=${linkName}">${linkName}</a></li>"""
+            }
+        }.mkString("<ul>", "\n", "</ul>")
+
+        s"""
+           |<html>
+           |<head><title>${name}</title></head>
+           |<body>
+           |  <h2>Inlinks:</h2>
+           |  ${inlinks}
+           |  <h2>Outlinks:</h2>
+           |  ${outlinks}
+           |</body>
+           |""".stripMargin
+    }
+  }
+
+  def showPageJson: Route = (request: Request, _: Response) => {
     val name = Option(request.params(":name")).getOrElse("").trim
     PageDb.getIdByName(name) match {
       case Some(pageId) =>
